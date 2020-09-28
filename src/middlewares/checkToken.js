808d5b53377f6264d05685bc46b5a6ea8a49f186
bhardwajhimanshu2515@gmail.com <bhardwajhimanshu2515@gmail.com>
2020-09-28 16:51:35 +0530

    himanshu || checkToken.js created

---
+++ src/middlewares/checkToken.js
@@ -0,0 +1,23 @@
+//import libs
+const jwt=require("jsonwebtoken");
+
+//import HttpResponse
+const HttpResponse=require("../models/http-response");
+
+//export module
+module.exports=(req,res,next)=>{
+    if(req.method==="OPTIONS"){
+        return next();
+    }
+    try{
+        const token=req.headers.authorization.split(" ")[1];
+        if(!token){
+            throw Error("Authentication Failed !!");
+        }
+        const decodedToken=jwt.verify(token,"myToken");
+        req.userData={userId:decodedToken.userId};
+    }catch(err){
+        const error=new HttpResponse("Authentication Failed",403);
+        return res.status(403).json({response:error});
+    }
+}
